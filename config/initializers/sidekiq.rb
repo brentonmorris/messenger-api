b8a0e7150908543ed81b6237d6aa1f9fb2cd11f3
Brenton Morris <brentonamorris@gmail.com>
2025-08-24 20:10:07 -0400

    Configure Sidekiq to no attempt to verify self-signed TLS certificate for Heroku redis

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,8 @@
 Sidekiq.configure_server do |config|
   config.redis = {
     url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0"),
-    network_timeout: 5
+    network_timeout: 5,
+    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
   }
   config.concurrency = 5
   config.queues = %w[default sms]
@@ -10,7 +11,8 @@ end
 Sidekiq.configure_client do |config|
   config.redis = {
     url: ENV.fetch("REDIS_URL", "redis://localhost:6379/0"),
-    network_timeout: 5
+    network_timeout: 5,
+    ssl_params: {verify_mode: OpenSSL::SSL::VERIFY_NONE}
   }
 end
 
